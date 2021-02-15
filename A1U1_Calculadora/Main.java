4551dbdaf2ea1b72b91d1172c4606f19599d3a9d
Jorge Antonio Toscano Lara <61440920+JorgeAntonioTL@users.noreply.github.com>
2021-02-15 09:50:16 -0600

    Add files via upload

---
+++ A1U1_Calculadora/Main.java
@@ -0,0 +1,10 @@
+//Jorge Antonio Toscano Lara
+package PruebaC2;
+
+public class Main {
+		public static void main(String[] args) {
+			Calculadora calculadora = new Calculadora();
+			calculadora.setVisible(true);
+		}
+	}
+
